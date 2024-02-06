fc2a7115ca3c75bfb789edeaa021549ae474eb8e
Karin Åstrand <karinastrand@yahoo.se>
2024-02-06 19:23:22 +0100

    Metoder i ProductList påbörjade

--- Productlist/ProductList.cs
+++ Productlist/ProductList.cs
@@ -12,11 +12,36 @@ internal class ProductList
 
     public void AddProducts()
     {
-        WriteLine();
+        WriteLine("Add products, Category, ProductName and Price, Price has to be an integer");
+        WriteLine("When you want to finish write q");
+        while (true) 
+        {
+            Write("Category (q to quit): ");
+            string category = ReadLine();
+            if (category.ToLower().Trim() == "q")
+            {
+                break;
+            }
+            Write("ProductName: ");
+            string name = ReadLine();
+            Write("Price: ");
+            string price = ReadLine();
+            int productPrice = 0;
+            Int32.TryParse(price, out productPrice);
+            Products.Add(new Product(category,name,productPrice));
+
+
+        }
+        
     }
     public void ShowProducts()
     {
-        WriteLine();
+        WriteLine("Category".PadLeft(10) + "Product".PadLeft(10) + "Price".PadLeft(10));
+        foreach (Product prod in Products) 
+        {
+            Console.WriteLine(PrintProduct(prod));
+        }
+        
     }
     public void SearchProducts()
     {
@@ -25,6 +50,11 @@ internal class ProductList
     public void sumPrice()
     {
 
+    }
+    public string PrintProduct(Product productToPrint)
+    {
+        
+        return $"{productToPrint.Category.PadLeft(10)} {productToPrint.ProductName.PadLeft(10)} {(productToPrint.Price).ToString().PadLeft(10)}";
     }
 
 }
